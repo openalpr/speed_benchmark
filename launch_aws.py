60fe29bb2a6a09c7dcc48c5c2b60b36f5b1cf1bd
Addison Klinke <agk38@case.edu>
2019-06-17 14:59:24 -0600

    CLI arg for version of setup script

--- launch_aws.py
+++ launch_aws.py
@@ -20,7 +20,7 @@ instances = {
     'z1d.large': {'cores': 1, 'cpu': 'Intel Xeon Platinum 8151 @ 3.4 GHz'}}
 
 
-def get_flags(ami, instance_type, key):
+def get_flags(ami, instance_type, key, version):
     """Format command line flags for AWS CLI.
 
     Output from the startup script is viewable on instance at
@@ -30,29 +30,37 @@ def get_flags(ami, instance_type, key):
     :param str ami: Image for instance.
     :param str instance_type: Type of AWS instance (i.e. t2.micro).
     :param str key: Name of key pair to assign to instance for SSH access.
+    :param str version: Either GA or latest OpenALPR.
     :return str flags:
     """
+    if version == 'GA':
+        setup = 'file://aws_setup_ga.sh'
+    elif version == 'latest':
+        setup = 'file://aws_setup_latest.sh'
+    else:
+        raise ValueError('Version must be GA or latest, received {}'.format(version))
     params = {
         'image-id': ami,
         'instance-type': instance_type,
         'key-name': key,
-        'user-data': 'file://aws_setup.sh',
+        'user-data': setup,
         'security-group-ids': 'sg-01463e7f4849905ee'}
     flags = '--' + ' --'.join(['{} {}'.format(k, v) if v is not None else k for k, v in params.items()])
     return flags
 
 
-def launch_instances(ami, instance_types, key):
+def launch_instances(ami, instance_types, key, version):
     """Allocate EC2 instances.
 
     :param str ami: Image for instance.
     :param [str] instance_types: List of AWS instance types (i.e. t2.micro).
     :param str key: Name of key pair to assign to instance for SSH access.
+    :param str version: Either GA or latest OpenALPR.
     :return [str] ids:
     """
     ids = []
     for instance in instance_types:
-        cmd = 'aws ec2 run-instances {}'.format(get_flags(ami, instance, key))
+        cmd = 'aws ec2 run-instances {}'.format(get_flags(ami, instance, key, version))
         out = subprocess.check_output(cmd.split())
         response = json.loads(out)
         ids.append(response['Instances'][0]['InstanceId'])
@@ -64,7 +72,7 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Benchmark OpenALPR speed on AWS instances')
     parser.add_argument('-a', '--ami', type=str, default='ami-024a64a6685d05041', help='image for instances')
     parser.add_argument('-k', '--key', type=str, default='aklinke', help='name of key pair for SSH access')
-    parser.add_argument('-v', '--version', type=str, required=True, help='software version of OpenALPR')
+    parser.add_argument('-v', '--version', type=str, required=True, help='either GA or latest OpenALPR')
     args = parser.parse_args()
 
     ids = launch_instances(args.ami, instances, args.key)
